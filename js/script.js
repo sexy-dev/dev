6fcce35d761cae14ab5615d4bb752fff6ae2a10a
Cuong Nguyen <cuongnv.9899@gmail.com>
2020-11-11 02:22:05 +0700

    ...

--- js/script.js
+++ js/script.js
@@ -144,5 +144,4 @@ document.querySelector('.view-more-btn').onclick = function() {
         text: 'I will update you soon!',
         icon: 'warning'
     })
-    window.location.href = "test.html";
 }
